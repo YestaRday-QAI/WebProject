0f438e455315751ba207e1d8b82483bac69ea4fc
YestaRday-QAI <y1105697391@gmail.com>
2023-01-16 11:57:10 +0800

    Update 1

---
+++ static/js/manage.js
@@ -0,0 +1,18 @@
+var list = document.querySelector('.list');
+var menus = list.querySelectorAll('.li_name');
+for (let i = 0; i < menus.length; i++) {
+    menus[i].addEventListener('click', function() {
+        alinks = this.nextElementSibling.children;
+        if (this.className == 'li_name') {
+            this.className = 'li_name current';
+            for (let k = 0; k < alinks.length; k++) {
+                alinks[k].style.height = '38px';
+            }
+        } else {
+            this.className = 'li_name';
+            for (let k = 0; k < alinks.length; k++) {
+                alinks[k].style.height = '0';
+            }
+        }
+    })
+}
